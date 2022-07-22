99314796e64e867e3b72824a5eb6c2df23678c9c
Henrique Lopes <henriquelopees7@gmail.com>
2022-07-21 22:31:25 -0300

    Cria ENUM para agrupar quais API's estão disponíveis

--- src/main/java/imdb/App.java
+++ src/main/java/imdb/App.java
@@ -7,25 +7,19 @@ import static com.diogonunes.jcolor.Ansi.colorize;
 import static com.diogonunes.jcolor.Attribute.BOLD;
 
 public class App {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String apikey = System.getenv("IMDB_API_KEY");
-        String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
-//        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=4";
+    public static void main(String[] args) throws IOException {
+        ContentAPI[] apis = ContentAPI.values();
         HTTPClient httpClient = new HTTPClient();
-
-        String body = httpClient.get(url);
-//        ContentExtractor nasaContentExtractor = new NasaContentExtractor();
-        ContentExtractor imdbContentExtractor = new ImdbContentExtractor();
-        List<Content> contents = imdbContentExtractor.extract(body);
-
         StickerGenerator stickerGenerator = new StickerGenerator();
-
-        for (Content content : contents) {
-            System.out.println("Título: " + colorize(content.title(), BOLD()));
-            System.out.println("Poster: " + colorize(content.imageUrl(), BOLD()));
-            stickerGenerator.generateSticker(content);
-            System.out.println("---");
+        for (ContentAPI api : apis) {
+            String body = httpClient.get(api.getApiUrl());
+            List<Content> contents = api.extract(body);
+            for (Content content : contents) {
+                System.out.println("Título: " + colorize(content.title(), BOLD()));
+                System.out.println("Poster: " + colorize(content.imageUrl(), BOLD()));
+                stickerGenerator.generateSticker(content);
+                System.out.println("---");
+            }
         }
-
     }
 }
---
+++ src/main/java/imdb/ContentAPI.java
@@ -0,0 +1,24 @@
+package imdb;
+
+import java.util.List;
+
+public enum ContentAPI {
+    IMDB("https://api.mocki.io/v2/549a5d8b/Top250Movies", new ImdbContentExtractor()),
+    NASA("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=4", new NasaContentExtractor());
+
+    private final String apiUrl;
+    private final ContentExtractor contentExtractor;
+
+    ContentAPI(String apiUrl, ContentExtractor contentExtractor) {
+        this.apiUrl = apiUrl;
+        this.contentExtractor = contentExtractor;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public List<Content> extract(String json) {
+        return this.contentExtractor.extract(json);
+    }
+}
